8a809444ebd9f1682dcf799fa82a3e6f8820a7c7
lawrencedsz9 <lawrencedsouza125@gmail.com>
2023-10-28 14:57:51 +0530

    created a basic UI for home page
    a basic UI page for home screen , with the necessary

--- src/assets/pages/home.jsx
+++ src/assets/pages/home.jsx
@@ -1,10 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Home() {
+  const [balance, setBalance] = useState(500); // Initial balance, you can change this as needed
+  const [expenseAmount, setExpenseAmount] = useState(0);
+  const [incomeAmount, setIncomeAmount] = useState(0);
+
+  // Function to add or remove balance
+  const updateBalance = (amount) => {
+    setBalance(balance + amount);
+  };
+
+  // Function to update expense and income amounts
+  const updateExpense = (amount) => {
+    setExpenseAmount(expenseAmount + amount);
+  };
+
+  const updateIncome = (amount) => {
+    setIncomeAmount(incomeAmount + amount);
+  };
+
   return (
-    <div>
-      <h1>Expense Tracker</h1>
-      <p>Welcome to the expense tracker app!</p>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold text-center mb-4">Expense Tracker</h1>
+      <div className="flex flex-col items-center">
+        <div className="bg-white rounded-lg shadow p-2 text-center">
+          <h2 className="text-lg font-semibold">Current Balance</h2>
+          <p className="text-2xl font-bold text-green-600">RS{balance.toFixed(2)}</p>
+          <div className="flex justify-between mt-2">
+            <button
+              className="px-2 py-1 bg-green-500 text-white rounded"
+              onClick={() => updateBalance(10)}
+            >
+              Credited
+            </button>
+            <button
+              className="px-2 py-1 bg-red-500 text-white rounded"
+              onClick={() => updateBalance(-10)}
+            >
+              Debited
+            </button>
+          </div>
+        </div>
+        <div className="mt-4 w-full">
+          <div className="flex justify-between">
+            <div className="bg-white rounded-lg shadow p-2 w-1/2">
+              <h2 className="text-lg font-semibold">Expense</h2>
+              <p className="text-2xl font-bold text-red-600">RS{expenseAmount.toFixed(2)}</p>
+            </div>
+            <div className="bg-white rounded-lg shadow p-2 w-1/2">
+              <h2 className="text-lg font-semibold">Income</h2>
+              <p className="text-2xl font-bold text-green-600">RS{incomeAmount.toFixed(2)}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-4">
+        <div className="bg-white rounded-lg shadow p-2">
+          <h2 className="text-lg font-semibold">Recent Activity</h2>
+          {/* You can display recent activity information here */}
+        </div>
+      </div>
     </div>
   );
 }
