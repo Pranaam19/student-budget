6215fa779c91cbdf9526f82825060d0d31edd5c0
Pranavv Raja <pranavvraja@gmail.com>
2023-11-06 20:50:26 +0530

    small changes in auth.jsx

--- src/assets/pages/auth.jsx
+++ src/assets/pages/auth.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 // import InputDialog from "../components/input";
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 import Input from "../components/input3";
-import { Button, Container, Dialog, Typography } from "@mui/material";
+import {
+  Button,
+  Container,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Typography,
+} from "@mui/material";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-import {auth} from "../firebase/firebase";
+import { auth } from "../firebase/firebase";
 import { EmailAuthProvider, GoogleAuthProvider } from "firebase/auth";
 
 const REDIRECT_PAGE = "/home";
@@ -13,22 +20,23 @@ const REDIRECT_PAGE = "/home";
 const uiConfig = {
   signInFlow: "popup",
   signInSuccessUrl: REDIRECT_PAGE,
-  signInOption: [
-    EmailAuthProvider.PROVIDER_ID,
-    GoogleAuthProvider.PROVIDER_ID,
-  ]
+  signInOption: [EmailAuthProvider.PROVIDER_ID, GoogleAuthProvider.PROVIDER_ID],
 };
 function Authentication() {
   const [login, setLogin] = useState(false);
   return (
     <div>
-    <div>
-      <Button onClick={() => setLogin(true)}>        
-        SignIn
-      </Button>
+      <div>
+        <Button onClick={() => setLogin(true)}>SignIn</Button>
       </div>
       <Dialog open={login} onClose={() => setLogin(false)}>
-      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth}></StyledFirebaseAuth>
+        <DialogTitle>Sign In</DialogTitle>
+        <DialogContent>
+          <StyledFirebaseAuth
+            uiConfig={uiConfig}
+            firebaseAuth={auth}
+          ></StyledFirebaseAuth>
+        </DialogContent>
       </Dialog>
     </div>
   );
